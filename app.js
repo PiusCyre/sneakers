f5a043d58104421494af8a582421eed75db16388
PiusCyre <108563817+PiusCyre@users.noreply.github.com>
2023-07-02 19:28:37 +0100

    lol

--- app.js
+++ app.js
@@ -29,54 +29,17 @@ closeModal.addEventListener("click", () => {
   overlay.classList.add("hidden");
 });
 
+const numberElement = document.querySelector(".number");
+const minusIcon = document.querySelector(".minus-icon");
+const plusIcon = document.querySelector(".plus-icon");
+const cartBtn = document.querySelector(".button");
 //for modal images
 pics.forEach(function (pic) {
   pic.addEventListener("click", (e) => {
     modalHeroImage.src = pic.src;
   });
 });
-
-// one hundred eaters, i cant fit them in one SUV!
-const imgs = [
-  "image-product-1.jpg",
-  "image-product-2.jpg",
-  "image-product-3.jpg",
-  "image-product-4.jpg",
-];
-// just wanted to destructure the array
-const [uno, dos, tres, quinto] = imgs;
-//now let us cook
-// for the previous and next button, but ill have to comment them out
-let currentImageIndex = 0;
-function updateImage() {
-  pics.src = imgs[currentImageIndex].src;
-}
-
-const prevImage = () => {
-  updateImage();
-  currentImageIndex--;
-  if (currentImageIndex < 0) {
-    currentImageIndex = imgs.length - 1;
-  }
-};
-const nextImage = () => {
-  updateImage();
-  currentImageIndex++;
-  if (currentImageIndex >= imgs.length) {
-    currentImageIndex = 0;
-  }
-};
-//event listeners now
-prevArrow.addEventListener("click", prevImage);
-nextArrow.addEventListener("click", nextImage);
-updateImage();
-
-// cart shit
-const numberElement = document.querySelector(".number");
-const minusIcon = document.querySelector(".minus-icon");
-const plusIcon = document.querySelector(".plus-icon");
-const cartBtn = document.querySelector(".button");
-
+// cart stuff
 let numeroPrimero = 0;
 const counterStuff = () => {
   numberElement.textContent = numeroPrimero;
@@ -89,7 +52,6 @@ minusIcon.addEventListener("click", function () {
   numeroPrimero--;
   counterStuff();
 });
-
 plusIcon.addEventListener("click", function () {
   numeroPrimero++;
   counterStuff();
@@ -124,3 +86,37 @@ exitBtn.addEventListener("click", function (event) {
 const timekeeper = document.querySelector(".time-element");
 const date = new Date();
 timekeeper.textContent = date.toDateString();
+
+const imgs = [
+  "image-product-1.jpg",
+  "image-product-2.jpg",
+  "image-product-3.jpg",
+  "image-product-4.jpg",
+];
+
+function updateImage() {
+  for (let i = 0; i <= imgs.length; i++) {
+    pics.src = imgs[currentImageIndex].src;
+  }
+}
+
+let currentImageIndex = 0;
+
+const prevImage = () => {
+  updateImage();
+  currentImageIndex--;
+  if (currentImageIndex < 0) {
+    currentImageIndex = imgs.length - 1;
+  }
+};
+const nextImage = () => {
+  updateImage();
+  currentImageIndex++;
+  if (currentImageIndex >= imgs.length) {
+    currentImageIndex = 0;
+  }
+};
+updateImage();
+//event listeners now
+prevArrow.addEventListener("click", prevImage);
+nextArrow.addEventListener("click", nextImage);
